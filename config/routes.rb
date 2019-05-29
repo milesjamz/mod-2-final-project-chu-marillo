8bd46db59a5cafea408efe6f94dcdff5634c126c
Miles Marillo <mjm03@hampshire.edu>
2019-05-29 09:43:11 -0400

    morning commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-root 'welcome#index'
+root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users, only:[:index, :show, :new, :create, :delete]
-get 'users/history', to: 'users#history'
+get 'users/profile', to: 'users#profile'
+get '/', to: 'sessions#create'
 resources :carts, only:[:show, :index, :new, :create, :edit, :update]
-resources :categories, only:[:show]
+# resources :categories, only:[:show]
 resources :items, only:[:show]
-resources :comments
+resources :comments, only:[:new, :create, :delete]
 
 end
