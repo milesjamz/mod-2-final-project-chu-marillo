5ceb291354784e133aa29738120c25d720088179
Aaron Chu <aaronchu357@gmail.com>
2019-05-30 17:30:54 -0400

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  resources :users
+  # get 'users', to: 'users#show', as: 'users'
+  resources :users#, except: [:index]
   resources :sessions, only:[:new, :create, :destroy]
   resources :line_items
 
