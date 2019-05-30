8958deeefd15208b03d8465d632e15764e08cbdc
Aaron Chu <aaronchu357@gmail.com>
2019-05-30 18:19:27 -0400

    Added current active cart helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
+  helper_method :current_user, :current_active_cart
 
   def current_user
     User.where(id: session[:user_id]).first
+  end
+
+  def current_active_cart
+    Cart.where(status: "active").first
   end
 end
